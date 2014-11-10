6384457144a93f44e5694e6d077ad990c1ab924e
Yoni Shalom <silver83@gmail.com>
2014-11-10 08:06:52 +0200

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,47 @@
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload');
-    webserver = require('gulp-webserver');
-    jsx = require('gulp-jsx');
+    livereload = require('gulp-livereload'),
+    webserver = require('gulp-webserver'),
+    jsx = require('gulp-jsx'),
+    watch = require('gulp-watch'),
+    path = require('path'),
+    splicer = require('labeled-stream-splicer');
 
-gulp.task('build', function() {
-   return gulp.src('js/*.js')
+var scripts = 'js/*.js',
+  other = '*.html',
+  pub = 'public/';
+
+var generic_pipe = splicer.obj([]);
+
+gulp.task('jsx', function() {
+    var dest = path.join(pub, 'js');
+    return gulp.src(scripts)
+        .pipe(watch(scripts))
+        .pipe(generic_pipe)
         .pipe(jsx())
-        .pipe(gulp.dest('js/compiled'));
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('other', function() {
+    var dest = pub;
+    return gulp.src(other)
+        .pipe(watch(other))
+        .pipe(generic_pipe)
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('./**').on('change', livereload.changed);
+    generic_pipe.push(livereload());
 });
 
 gulp.task('webserver', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
 });
 
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'webserver', 'build']); //, 'scripts', 'images']);
+gulp.task('default', [ 'webserver', 'jsx', 'other', 'watch'  ]); //, 'scripts', 'images']);
